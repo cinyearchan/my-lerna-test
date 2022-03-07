0538ebeda12b608ad3689d22b1b0cd9e80277613
cinyearchan <cinyearchan@gmail.com>
2022-03-07 14:12:41 +0800

    init

---
+++ packages/admin-site/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WaterForm from '@my-lerna-test/common/components/WaterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>管理员站点</h2>
+      <WaterForm></WaterForm>
+    </div>
+  );
+}
+
+export default App;
